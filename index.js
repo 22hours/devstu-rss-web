2c7845eb957142f1593b4c863be4de6dd9661937
winterlood <king199777@gmail.com>
2020-07-10 20:51:20 +0900

    [FEAT] : rss routing 완료

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const DOMAIN = `http://34.64.92.51:80/`;
+const FIND_RSS_QUESTIONS = "main/question/find/rss";
+const GET = async (method, url, data) => {
+    const axios = require("axios").default;
+    var authorization = "guest";
+    return axios({
+        method,
+        url: DOMAIN + url,
+        data,
+        headers: { Authorization: authorization },
+    })
+        .then((result) => result.data)
+        .catch((result) => {
+            console.log(result);
+            return null;
+        });
+};
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine','ejs');
-app.use(express.static(__dirname + '/public'));
+const doAsync = (fn) => async (req, res, next) => await fn(req, res, next).catch(next);
 
-app.get('/hello', function(req,res){
-  res.render('hello', {name:req.query.nameQuery});
-});
+async function err() {
+    throw new Error("에러 발생");
+}
 
-app.get('/hello/:nameParam', function(req,res){
-  res.render('hello', {name:req.params.nameParam});
-});
+//라우팅 핸들러
+app.get(
+    "/rss",
+    doAsync(async (req, res) => {
+        var item = null;
+        var data = await GET("get", FIND_RSS_QUESTIONS);
+        if (data) {
+            item = data.map((it) => {
+                return {
+                    title: it.title,
+                    id: `https://devstu.co.kr/howto/question/${it._id}`,
+                    url: `https://devstu.co.kr/howto/question/${it._id}`,
+                    pubDate: it.date,
+                };
+            });
+            const jsonfeedToRSS = require("jsonfeed-to-rss");
+
+            var lastBuildDate = new Date();
+            var testJSON = {
+                version: "https://jsonfeed.org/version/1",
+                title: "DEVSTU",
+                home_page_url: "https://devstu.co.kr",
+                feed_url: "https://jsonfeed-to-rss.netlify.com/snapshots/readme-feed.json",
+                description: "검색해도 도저히 모르겠다면, DESVTU",
+                next_url: "https://jsonfeed-to-rss.netlify.com/snapshots/2017.json",
+                icon: "https://jsonfeed-to-rss.netlify.com/icon-512x512.png",
+                items: item,
+            };
+            const rssFeed = jsonfeedToRSS(testJSON);
+
+            res.set("Content-Type", "text/xml");
+            res.send(rssFeed);
+        } else {
+            res.render("hello", { name: req.query.nameQuery });
+        }
+    })
+);
 
 var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+app.listen(port, function () {
+    console.log("server on! http://localhost:" + port);
 });
