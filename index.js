9ab254c90da7de0078bd30ac72f00b4f6ce2beb5
winterlood <king199777@gmail.com>
2020-07-10 21:59:07 +0900

    [FEAT] : devlog rss 추가

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
-const DOMAIN = `http://34.64.92.51:80/`;
+const DOMAIN = "https://devstu.fdsafdsa.shop/";
 const FIND_RSS_QUESTIONS = "main/question/find/rss";
+const FIND_ALL_DEVL_LOGS = "main/devlog/find/all";
 const GET = async (method, url, data) => {
     const axios = require("axios").default;
     var authorization = "guest";
@@ -32,7 +33,12 @@ app.get(
     doAsync(async (req, res) => {
         var item = null;
         var data = await GET("get", FIND_RSS_QUESTIONS);
-        if (data) {
+        var data2 = await GET("post", FIND_ALL_DEVL_LOGS, {
+            param: "date",
+            pageNum: "1",
+            requiredCount: "10",
+        });
+        if (data && data2) {
             item = data.map((it) => {
                 return {
                     title: it.title,
@@ -41,8 +47,16 @@ app.get(
                     date_published: it.date,
                 };
             });
+            item2 = data2.map((it) => {
+                return {
+                    title: it.title,
+                    id: `https://devstu.co.kr/devlog/logs/${it._id}`,
+                    url: `https://devstu.co.kr/devlog/logs/${it._id}`,
+                    date_published: it.date,
+                };
+            });
             const jsonfeedToRSS = require("jsonfeed-to-rss");
-
+            total = item2.concat(item);
             var lastBuildDate = new Date();
             var testJSON = {
                 version: "https://jsonfeed.org/version/1",
@@ -52,7 +66,7 @@ app.get(
                 description: "검색해도 도저히 모르겠다면, DESVTU",
                 next_url: "https://jsonfeed-to-rss.netlify.com/snapshots/2017.json",
                 icon: "https://jsonfeed-to-rss.netlify.com/icon-512x512.png",
-                items: item,
+                items: total,
             };
             const rssFeed = jsonfeedToRSS(testJSON);
 
